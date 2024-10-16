c35510eb7eac3cb770e51af3b74a87b77f9ca734
mike-el-fenixxx <mike.el.fenixxx@gmail.com>
2024-10-16 01:12:46 -0400

    Pushing entire project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { animals } from "./data/animals.js";
+import React, { useState } from "react";
+import { createRoot } from "react-dom/client";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [fact, setFact] = useState('');
+    const [title, setTitle] = useState('');
+
+    const displayFact = (animal) => {
+        const animalFacts = animals[animal].facts;
+        const randomIndex = Math.floor(Math.random() * animalFacts.length);
+        const funFact = animalFacts[randomIndex];
+        setFact(funFact);
+        setTitle(animal);
+    };
+
+    const background = (
+        <div className="background" style={{ backgroundColor: 'teal' }} />
+    );
+
+    const showBackground = true;
+
+    const animalImages = Object.keys(animals).map((animal) => (
+        <img
+            key={animal}
+            className="animal"
+            alt={animal}
+            src={animals[animal].image}
+            aria-label={animal}
+            role="button"
+            onClick={() => displayFact(animal)}
+        />
+    ));
+
+    return (
+        <div>
+            <h1>{title === "" ? "Click an animal for a fun fact" : `${title} fact`}</h1>
+            {showBackground && background}
+            <div className="animals">
+                {animalImages}
+            </div>
+            <p id='fact'>{fact}</p>
+        </div>
+    );
+};
+
+const container = document.getElementById("app");
+const root = createRoot(container);
+root.render(<App />);
+
 
 export default App;
